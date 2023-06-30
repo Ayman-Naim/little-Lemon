bbf024abe9d0fa0221ebb3a84de70e8f34e8e48e
ayman <ayman@MacBook-Pro.local>
2023-06-30 22:31:25 +0300

    first commit

---
+++ Little-Lemon-Food-Ordering-App/Little_Lemon_Food_Ordering_AppApp.swift
@@ -0,0 +1,20 @@
+//
+//  Little_Lemon_Food_Ordering_AppApp.swift
+//  Little-Lemon-Food-Ordering-App
+//
+//  Created by ayman on 30/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemon_Food_Ordering_AppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# Little-Lemon-dinner-menu 
+it's a project practicing swiftui in meta ios Meta iOS Developer Professional Certificate
+
+its a project for creating menu for restaurant 
+
+
+vedio Demo :
+
+
+
+<img src="https://github.com/Ayman-Naim/Little-Lemon-dinner-menu/blob/main/screenshots/Demo.gif">
+
+
+screenshots:
+
+![](screenshots/1.png)
+
+
+![](screenshots/2.png)
+
+
+![](screenshots/3.png)
+
+
+![](screenshots/4.png)
+
+
+![](screenshots/5.png)
